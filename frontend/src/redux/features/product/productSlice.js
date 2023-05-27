fee56a4d087897c82c3dd267e33408ef3f4fa715
Ankit Yadav <119873198+ANKITy102@users.noreply.github.com>
2023-05-27 18:50:08 +0530

    created all the function required for add product like image, productImage, onChangeHandler and also the create product route is created through redux store

--- frontend/src/redux/features/product/productSlice.js
+++ frontend/src/redux/features/product/productSlice.js
@@ -1,4 +1,6 @@
 import {createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import productService from './productService';
+import { toast } from 'react-toastify';
 
 const initialState = {
     product: null,
@@ -9,6 +11,23 @@ const initialState = {
     message:"",
 }
 
+//Create New Product
+export const createProduct = createAsyncThunk(
+    "products/create",
+    async(formData, thunkAPI)=>{
+        try{
+            return await productService.createProduct(formData);
+        }catch(error){
+        const message = (error.response && error.response.data && error.response.data.message) || error.message || error.toString();
+            console.log(message);
+            return thunkAPI.rejectWithValue(message);
+        }
+    }
+)
+//Now the above request can have three state pending, success, error
+// to capture all of the states, we need to head over to our extra reducers.
+
+
 const productSlice = createSlice({
     name: "product",
     initialState,
@@ -18,11 +37,30 @@ const productSlice = createSlice({
         }
     },
     extraReducers:(builder)=>{
-
+        builder
+        .addCase(createProduct.pending, (state)=>{
+            state.isLoading = true
+        })
+        .addCase(createProduct.fulfilled, (state, action)=>{
+            state.isLoading = false;
+            state.isSuccess = true;
+            console.log(action.payload);
+            state.products.push(action.payload);
+            toast.success("Product added successfully");
+        })
+        .addCase(createProduct.rejected, (state, action)=>{
+            state.isLoading = false;
+            state.isError = true;
+            state.message = action.payload;
+            toast.error(action.payload);
+        })
     }
 });
 
 export const {CALC_STORE_VALUE} = productSlice.actions
 
+export const selectIsLoading = (state) =>{
+    return state.product.isLoading;
+}
 
 export default productSlice.reducer
